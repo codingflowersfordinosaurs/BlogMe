7bdf58e13330b63382534629b6905110e3b8ba49
Plant Lady <codeplantsbylouie@gmail.com>
2019-05-17 10:56:02 -0400

    basic CRUD pattern established

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
 let express = require('express');
+let exphbs = require("express-handlebars");
 
 let app = express();
+
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
 
 let PORT = process.env.PORT || 3000;
 
 // ROUTES
 app.get("/", async (req,res) => {
-  console.log(req);
 
-  res.send("hello world!");
+  res.render("home");
+});
+app.get("/pages", async (req, res) => {
+
+});
+app.get("/pages/:id", async (req, res) => {
+
+});
+
+app.post("/pages", async (req, res) => {
+
+});
+app.put("/pages/:id", async (req, res) => {
+
+});
+app.delete("/pages/:id", async (req, res) => {
+
 });
 
 
 
-
-app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`App listening on port ${PORT}; http://localhost:3000`));
